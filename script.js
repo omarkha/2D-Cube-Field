adf033b253837a0a9b62dd6a714df60e571a20c5
Mac <mac@Macs-MBP.lan>
2022-03-07 18:59:21 -0500

    added 2 functions. start game and move obstacles.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let realtimeScore = 0;
 let scoreList = [];
 let blockPositions = [];
 let topScore = 0;
+
+// Functions
+
+const startGame = function(){
+
+}
+
+const moveObstacles = function(){
+
+}
+
