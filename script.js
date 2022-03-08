8e951f9dce6fbba76d7a6bb63e107b0eb5752f25
Mac <mac@Macs-MBP.lan>
2022-03-07 20:02:19 -0500

    added ids to the blocks, modified the list of availablePositions and modified the emergeObstacles function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 
 let realtimeScore = 0;
 let scoreList = [];
-let blockPositions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
+let availablePositions = ['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13',
+'s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29',
+'s30','s31','s32','s33','s34','s35','s36','s37','s38','s39'];
 let topScore = 0;
 
 const board = document.querySelector(".game-board");
@@ -25,7 +27,22 @@ const shiftObstacles = function(){
 
 }
 
-const obstacleEmerge = function(){
+const emergeObstacles = function(){
+
+
+    /* the below code does the following:
+    constant x hold the value of a random number between 0 and the length of the
+    avaialable positions array.
+    if the array includes the position that was randomized then the board will recieve
+    an obstacle in that position; then the position is temporarily removed from the array as a possible spot.
+    */
+
+    const x = Math.ceil(Math.random() * availablePositions.length) -1;
+    console.log(x);
+    if(availablePositions.includes(availablePositions[x])){
+        board.children[x].setAttribute("class","obstacle");
+        choices.splice(availablePositions.indexOf(availablePositions[x]),1);
+    }
     
 }
 
@@ -51,6 +68,7 @@ addEventListener('keydown', (e) => {
             break;
 
         case 32 :
+            emergeObstacles();
             alert("space");
             break;
 
