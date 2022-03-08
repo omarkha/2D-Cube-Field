0f654b2bbf35a6e1885fada67243d513a789eb67
Mac <mac@Macs-MBP.lan>
2022-03-07 19:30:01 -0500

    filled the blockPositions

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 
 let realtimeScore = 0;
 let scoreList = [];
-let blockPositions = [];
+let blockPositions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
 let topScore = 0;
 
 const board = document.querySelector(".game-board");
 const arrow = document.querySelector("#arrow");
 const body = document.body;
+
 
 // Functions
 
@@ -25,7 +26,7 @@ const shiftObstacles = function(){
 }
 
 const obstacleEmerge = function(){
-
+    
 }
 
 const pauseGame = function(){
