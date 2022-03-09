c2edb1fa2ab4c708580dad7167bd0791e2be46cf
Mac <mac@Macs-MBP.lan>
2022-03-09 00:18:33 -0500

    working on it

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ let availablePositions = ['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s1
 ,'s81','s82','s83','s84','s85','s86','s87','s88','s89','s90','s91','s92'];
 let topScore = 0;
 let boardObstacles = [];
+let boardObstacles2 = [];
 let arrowleft = 0;
 let reachedBottom = false;
+let secondPatchActivated = true;
 let downkey = {
     direction: null,
     down: false
@@ -33,6 +35,7 @@ class Obstacles {
 }
 
 let obstacleObjects = [];
+let obstacleObjects2 = [];
 
 const board = document.querySelector('.game-board');
 const arrow = document.querySelector('#arrow');
@@ -49,21 +52,14 @@ const runningTime = () =>{
         
     
 }
-
-const gameRotation = function(){
-   for(let i=0 ; i < 8 ; i++){
-        setTimeout(emergeObstacles(), 236);
-        
-   }
-        
-}
+ 
 
 
 const startGame = function(moveOb, runningT){
     pause = false;
-    gameRotation();
     setInterval(moveObstacles, moveOb);
     setInterval(runningTime, runningT);
+    setTimeout(emergeObstacles, 1000);
 }
 
 
@@ -71,39 +67,181 @@ const startGame = function(moveOb, runningT){
 
 
 const clearObstacles = function(obstacle){
+    
+    if(obstacleObjects.includes(obstacle)){
     obstacle.top = 0;
     document.querySelector("#"+obstacle.id).setAttribute("class","");
     document.querySelector("#"+obstacle.id).style = "position: relative; top: " + obstacle.top + "px;";
     obstacleObjects.splice(obstacleObjects.indexOf(obstacle),1);
+    }else{
+        obstacle.top = 0;
+        document.querySelector("#"+obstacle.id).setAttribute("class","");
+        document.querySelector("#"+obstacle.id).style = "position: relative; top: " + obstacle.top + "px;";
+        obstacleObjects2.splice(obstacleObjects2.indexOf(obstacle),1);
+    }
 }
+let allowedToEmerge1 = true;
+let allowedToEmerge2 = false;
 
 const checkRemerge = function() {
     
     
+    
     let lowestPos = 0;
+
+    if((boardObstacles.length === 8 && allowedToEmerge1) || (!allowedToEmerge2 && allowedToEmerge1 && reemerging)){
 
     let array = obstacleObjects.map(element => {
         return parseInt(element.id.replace('s',''));
     })
 
+    reemerging = true;
     
     
     array.sort();
 
-    lowestPos = array[0];
+        lowestPos = array[0];    
 
-    
+
     let lowestObst = document.querySelector('#s'+lowestPos).getBoundingClientRect();
     
-    let basePoint = document.querySelector('#s85').getBoundingClientRect();
-    
+    let basePoint = document.querySelector('#s99').getBoundingClientRect();
     let isOverlappingBase = basePoint.bottom + basePoint.height < lowestObst.bottom; 
         
         if(isOverlappingBase){
+            allowedToEmerge2 = true;
+            allowedToEmerge1 = false;
             reemerging = true;
             emergeObstacles();
         }
+        
+     }else if(!allowedToEmerge1 && allowedToEmerge2 && reemerging){
 
+       
+        let array2 = obstacleObjects2.map(element => {
+            return parseInt(element.id.replace('s',''));
+        })
+    
+        
+        
+        array2.sort();
+    
+        let lowestPos2 = array2[0];
+    
+    
+        let lowestObst2 = document.querySelector('#s'+lowestPos2).getBoundingClientRect();
+        
+        let basePoint2 = document.querySelector('#s99').getBoundingClientRect();
+        
+        let isOverlappingBase2 = basePoint2.bottom + basePoint2.height < lowestObst2.bottom; 
+
+            if(isOverlappingBase2){
+                
+                reemerging = true;
+                allowedToEmerge1 = true;
+                allowedToEmerge2 = false;
+                emergeObstacles();
+            }
+    }
+}
+
+const emergeObstacles = function(){
+
+
+
+   const newObstacle = new Obstacles(posID,0,0);
+                // set time out here for a pause between displays
+
+                    let randColor = "obs-"+Math.ceil(Math.random() * 3);
+                    let posID = availablePositions[Math.ceil(Math.random() * availablePositions.length) -1];
+                    boardObstacles.push(newObstacle.id);
+                    obstacleObjects.push(newObstacle);
+                    console.log("adding "+newObstacle.id);
+                    document.querySelector("#"+newObstacle.id).setAttribute("class","obstacle "+randColor);
+
+
+    /*
+            if(allowedToEmerge1 && !allowedToEmerge2){
+                if(boardObstacles.length === 0 || (boardObstacles.length > 0 && reemerging)){
+                    for(let x=0;x<8;x++){
+
+                    
+                    let randColor = "obs-"+Math.ceil(Math.random() * 3);
+                    let posID = availablePositions[Math.ceil(Math.random() * availablePositions.length) -1];
+
+                if(!boardObstacles.includes(posID)){
+                    if(boardObstacles.length === 8 && reemerging){
+                        x=8;
+                        allowedToEmerge2 = true;
+                        allowedToEmerge1 = false;
+                        reemerging = true;
+                        console.log("oB1 emergefunction");
+                        console.log(`allowedToEmerge1 = ${allowedToEmerge1} and allowedToEmerge2 = ${allowedToEmerge2}`);
+                }else{
+                    const newObstacle = new Obstacles(posID,0,0);
+                // set time out here for a pause between displays
+                    boardObstacles.push(newObstacle.id);
+                    obstacleObjects.push(newObstacle);
+                    console.log("adding "+newObstacle.id);
+                    document.querySelector("#"+newObstacle.id).setAttribute("class","obstacle "+randColor);
+                    }
+                }
+            }
+                
+                
+            }
+            }else if(allowedToEmerge2 && !allowedToEmerge1 && reemerging){
+                
+                if(boardObstacles2.length === 0 || (boardObstacles2.length > 0 && reemerging)){
+                for(let x=0;x<8;x++){
+
+                
+                 randColor = "obs-"+Math.ceil(Math.random() * 3);
+                 posID = availablePositions[Math.ceil(Math.random() * availablePositions.length) -1];
+
+                if(!boardObstacles2.includes(posID)){
+                    if(boardObstacles2.length === 8){
+                        x=8;
+                        allowedToEmerge1 = true;
+                        allowedToEmerge2 = false;
+                        reemerging = true;
+                        console.log("oB2 emergefunction");
+                        console.log(`allowedToEmerge1 = ${allowedToEmerge1} and allowedToEmerge2 = ${allowedToEmerge2}`);
+                }else{
+                    const newObstacle2 = new Obstacles(posID,0,0);
+                boardObstacles2.push(newObstacle2.id);
+                obstacleObjects2.push(newObstacle2);
+                console.log("adding "+newObstacle2.id);
+                document.querySelector("#"+newObstacle2.id).setAttribute("class","obstacle "+randColor);
+                }
+                
+            }
+            
+                
+            }
+            }
+
+          }
+            
+        
+        */
+}
+       
+
+const pauseGame = function(bool){
+    pause = bool;
+}
+
+const gameOver = function(){
+    scoreList.push(realtimeScore);
+
+    scoreList.forEach(element => {
+        if(element > topScore){
+            topScore = element;
+        }
+    });
+
+    document.querySelector('#top-score').innerText = topScore;
 
 }
 
@@ -140,7 +278,34 @@ const checkCollision = function(objCollidedWith){
             clearObstacles(element);
         }
     });
+    obstacleObjects2.forEach(element => {
+        
+        let obstacle2 = document.querySelector('#'+element.id).getBoundingClientRect();
 
+
+       
+       const isInHoriztonalBounds =
+            arrow.left < obstacle2.left + obstacle2.width && arrow.left + arrow.width > obstacle2.left;
+        const isInVerticalBounds =
+            arrow.top < obstacle2.top + obstacle2.height && arrow.top + arrow.height > obstacle2.top;
+        const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
+       
+        const isInHoriztonalBoundsBoard =
+        board.left < obstacle2.left + obstacle2.width && board.left + board.width > obstacle2.left;
+        const isInVerticalBoundsBoard =
+        board.bottom < obstacle2.top + obstacle2.height && board.bottom + board.height > obstacle2.top;
+        const isOverlappingBoard = isInHoriztonalBoundsBoard && isInVerticalBoundsBoard;
+        
+        if(isOverlapping){
+            alert("collision");
+            pause = true;
+            gameOver();
+        }
+        
+        if(isOverlappingBoard){
+            clearObstacles(element);
+        }
+    });
 }
 
 const moveObstacles = function(){
@@ -148,6 +313,13 @@ const moveObstacles = function(){
         let obj = null;
         if(pause === false){
         obstacleObjects.forEach(element => {
+            element.top++;
+            obj = document.querySelector("#"+element.id).getBoundingClientRect();
+            element = 
+            document.querySelector("#"+element.id).style = "position: relative; top: " + element.top + "px;";
+            
+        });
+        obstacleObjects2.forEach(element => {
             element.top++;
             obj = document.querySelector("#"+element.id).getBoundingClientRect();
             element = 
@@ -194,49 +366,7 @@ const shiftArrow = function(){
     
 }
 
-const emergeObstacles = function(){
 
-
-    if(reemerging === false){
-        x = 8;
-     }else{
-        x = 16;
-     }
-
-        while(boardObstacles.length < x || (reemerging === true && boardObstacles.length < 16)){
-
-            let randColor = "obs-"+Math.ceil(Math.random() * 3);
-            let posID = availablePositions[Math.ceil(Math.random() * availablePositions.length) -1];
-        if(boardObstacles.includes(posID)){
-            console.log("includes " + posID);
-        }else{
-            const newObstacle = new Obstacles(posID,0,0);
-            boardObstacles.push(newObstacle.id);
-            obstacleObjects.push(newObstacle);
-            console.log("adding "+newObstacle.id);
-            document.querySelector("#"+newObstacle.id).setAttribute("class","obstacle "+randColor);
-        }
-
-        }
-    
-}
-
-const pauseGame = function(bool){
-    pause = bool;
-}
-
-const gameOver = function(){
-    scoreList.push(realtimeScore);
-
-    scoreList.forEach(element => {
-        if(element > topScore){
-            topScore = element;
-        }
-    });
-
-    document.querySelector('#top-score').innerText = topScore;
-
-}
 
 
 const moveArrow = (int) => {
