d973587600297ba769c1443c4a966c626cfac589
Mac <mac@Macs-MBP.lan>
2022-03-11 10:22:19 -0500

    fixed overlapping obstacles bug

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let availablePositions = ['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s1
 's14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24','s25','s26','s27','s28','s29',
 's30','s31','s32','s33','s34','s35','s36','s37','s38','s39','s40','s41','s42','s43','s44','s45','s46'
 ,'s47','s48','s49','s50','s51','s52','s53','s54','s55','s56','s57','s58','s59','s60','s61','s62','s63'
-,'s64','s65','s66','s67','s68','s69','s70','s71','s72','s73','s74','s75','s76','s77','s78','s79'];
+,'s64','s65','s66','s67','s68','s69','s70','s71','s72','s73','s74','s75','s76','s77','s78'];
 let topScore = 0;
 let boardObstacles = [];
 let arrowLeft = 0;
@@ -52,6 +52,7 @@ class Obstacles {
         this.id = id;
         this.top = top;
         this.left = left;
+        this.topr = null;
     }
 }
 
@@ -234,7 +235,7 @@ const startGame = function(){
      emergenceTimer3 = setInterval(emergeObstacles, 16818);
      timeMovement = setInterval(runningTime, 24);
      
-    message.innerText = `Game Started: ${difficulty}`;
+    message.innerText = `Game Started..  ${difficulty} mode`;;
 
     let arrowRunner = function() {
         setTimeout(shiftArrow, -20);
@@ -271,21 +272,78 @@ const emergeObstacles = function(){
         for(let x=0;x<randomNum;x++){
 
             if(night){
-                randColor = "obs-"+Math.ceil(Math.random() * 3);
+                randColor = "obs-"+Math.ceil(Math.random() * 5);
             }else{
-                randColor = "obsd-"+Math.ceil(Math.random() * 3);
+                randColor = "obsd-"+Math.ceil(Math.random() * 5);
             }
                         
             let posID = availablePositions[Math.ceil(Math.random() * availablePositions.length) -1];
                         
             let newObstacle = new Obstacles(posID,0,0);
+            
+            let newObstacleRect = document.querySelector('#'+newObstacle.id).getBoundingClientRect();
 
+            
             if(!boardObstacles.includes(newObstacle.id)){
+                document.querySelector("#"+newObstacle.id).setAttribute("class","obstacle "+randColor);
                 boardObstacles.push(newObstacle.id);
                 obstacleObjects.push(newObstacle);
                 console.log("adding "+newObstacle.id);
-                document.querySelector("#"+newObstacle.id).setAttribute("class","obstacle "+randColor);
             }
+
+            let overlapped = false;
+
+            boardObstacles.forEach(element => {
+                if(element != newObstacle.id){
+
+                let spot = document.querySelector('#'+element).getBoundingClientRect()
+                ;
+
+              
+                const isInHoriztonalBounds =
+                spot.left < newObstacleRect.left + newObstacleRect.width && spot.left + spot.width > newObstacleRect.left;
+                const isInVerticalBounds =
+                spot.top < newObstacleRect.top + newObstacleRect.height && spot.top + spot.height > newObstacleRect.top;
+                const isOverlapping = isInHoriztonalBounds && isInVerticalBounds; 
+                
+                if(isOverlapping){
+                    clearObstacles(newObstacle);
+                    
+                }
+            }
+            });
+            /*
+            availablePositions.forEach(element => {
+
+                let spot = document.querySelector('#'+element).getBoundingClientRect;
+
+                if(document.querySelector('#'+element).getAttribute("class") === "obstacle" || document.querySelector('#'+element).getAttribute("class") === "obstacle obs-1" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obs-2" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obs-3" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obs-4" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obs-5" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obsd-1" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obsd-2" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obsd-3" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obsd-4" || 
+                document.querySelector('#'+element).getAttribute("class") === "obstacle obsd-5" ){
+
+
+                const isInHoriztonalBounds =
+                spot.left < newObstacle.left + newObstacle.width && spot.left + spot.width > newObstacle.left;
+                const isInVerticalBounds =
+                spot.top < newObstacle.top + newObstacle.height && spot.top + spot.height > newObstacle.top;
+                const isOverlapping = isInHoriztonalBounds && isInVerticalBounds; 
+                
+                if(isOverlapping){
+                    alert();
+                    overlapped = true;
+                }
+
+            }
+            
+            });
+            */
 
         }
 
@@ -300,7 +358,7 @@ const checkTopScoreSurpassed = () => {
         topscoreSurpassedSound.play();
         message.innerText = "Congrats! Top Score Surpassed!";
         document.querySelector(".current-score").setAttribute("id","current-score-surpassed");
-        setTimeout(() => {message.innerText = `Game Started: ${difficulty}`;}, 5000);
+        setTimeout(() => {message.innerText = `Game Started.. ${difficulty} mode`}, 5000);
     }
 }
 
@@ -324,10 +382,10 @@ const checkCollision = function(objCollidedWith){
     let board = document.querySelector('.game-board').getBoundingClientRect();
 
     obstacleObjects.forEach(element => {
-        
+
         let obstacle = document.querySelector('#'+element.id).getBoundingClientRect();
 
-
+        element.topr = obstacle.top;
        
        const isInHoriztonalBounds =
             arrow.left < obstacle.left + obstacle.width && arrow.left + arrow.width > obstacle.left;
@@ -351,6 +409,9 @@ const checkCollision = function(objCollidedWith){
         if(isOverlappingBoard && !gameEnded){
             clearObstacles(element);
         }
+
+        const obstacleBound = obstacle.bottom + obstacle.height 
+        
     });
 }
 
@@ -423,7 +484,6 @@ const shiftArrow = function(){
 window.addEventListener("load", () => { 
 
     gamethemeSound.setVolume(62 / 100);
-
 });
 
 addEventListener('keydown', (e) => {
@@ -453,13 +513,15 @@ addEventListener('keydown', (e) => {
                     gamethemeSound.play();
                 }else if(pause === false && gameStarted === true && !gameEnded){
                     pauseGame(true);
-                    gamethemeSound.pause();
+                    
                 }
                 
             break;
         case 13 :
             if((!gameEnded && !gameStarted) || gameReset){
                 startGame();
+            }else if(!gameReset && gameEnded){
+                resetGame();
             }
             break;
         default:
@@ -498,7 +560,7 @@ document.querySelector(".easy").addEventListener('click',function(){
     if((gameEnded && !gameStarted) || (gameEnded || !gameStarted)){
         message.innerText = `Game set to  ${difficulty}`;
     }else{
-        message.innerText = `Game Started:  ${difficulty}`;
+        message.innerText = `Game Started.. ${difficulty} mode`;
     }
     this.setAttribute("id","mode-pressed");
     document.querySelector(".medium").setAttribute("id",null);
@@ -511,7 +573,7 @@ document.querySelector(".medium").addEventListener('click',function(){
     if((gameEnded && !gameStarted) || (gameEnded || !gameStarted)){
         message.innerText = `Game set to  ${difficulty}`;
     }else{
-        message.innerText = `Game Started:  ${difficulty}`;
+        message.innerText = `Game Started.. ${difficulty} mode`;
     }
     document.querySelector(".easy").setAttribute("id",null);
     document.querySelector(".hard").setAttribute("id",null);
@@ -525,7 +587,7 @@ document.querySelector(".hard").addEventListener('click',function(){
     if((gameEnded && !gameStarted) || (gameEnded || !gameStarted)){
         message.innerText = `Game set to  ${difficulty}`;
     }else{
-        message.innerText = `Game Started:  ${difficulty}`;
+        message.innerText = `Game Started.. ${difficulty} mode`;
     }
     document.querySelector(".easy").setAttribute("id",null);
     document.querySelector(".medium").setAttribute("id",null);
